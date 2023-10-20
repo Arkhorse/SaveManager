e3ae5c57ef39410f21d530406307978967c76be7
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-20 20:54:34 -0300

    rename

---
+++ VisualStudio/Utilities/CommonUtilities.cs
@@ -0,0 +1,30 @@
+﻿using SaveManager.Utilities;
+
+namespace SaveManager
+{
+    internal class CommonUtilities
+    {
+        /// <summary>
+        /// When the save get is pressed, this gets called
+        /// </summary>
+        internal static void OnSaveKey()
+        {
+            if (Settings.Instance.EnableMod && Il2Cpp.InputManager.instance != null && !GameManager.IsStoryMode())
+            {
+                GameManager.SaveGameAndDisplayHUDMessage();
+            }
+        }
+
+        internal static void OnLoadKey()
+        {
+            if (Settings.Instance.EnableMod && Il2Cpp.InputManager.instance != null && !GameManager.IsStoryMode())
+            {
+                SaveSlotInfo ssi = SaveGameSlotHelper.GetCurrentSaveSlotInfo();
+                string slotname = ssi.m_SaveSlotName;
+                int version = ssi.m_SaveChangelistVersion;
+
+                GameManager.LoadSaveGameSlot(slotname, version);
+            }
+        }
+    }
+}
