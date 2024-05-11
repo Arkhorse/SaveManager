9834631aaf9367ee971883c15878d9a307f08dc5
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-11 19:13:33 -0300

    Add the ability to load and autosave

--- VisualStudio/BuildInfo.cs
+++ VisualStudio/BuildInfo.cs
@@ -8,7 +8,7 @@
         /// <summary>Who made the mod</summary>
         public const string Author                          = "The Illusion";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "1.0.1";
+        public const string Version                         = "1.0.4";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Save Manager";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
--- VisualStudio/SaveManager.cs
+++ VisualStudio/SaveManager.cs
@@ -1,26 +1,72 @@
-﻿namespace SaveManager
+﻿using System.Collections;
+
+using Il2CppInterop.Runtime.Attributes;
+
+using MelonLoader;
+
+namespace SaveManager
 {
-    public class SaveManager : MelonMod
-    {
-        public static string CurrentSaveName { get; set; } = string.Empty;
-        public override void OnInitializeMelon()
-        {
-            Settings.OnLoad();
-        }
+	public class SaveManager : MelonMod
+	{
+		public static string CurrentSaveName { get; set; } = string.Empty;
+		public static object coroutine;
+		public static bool saving;
+		public override void OnInitializeMelon()
+		{
+			Settings.OnLoad();
+		}
 
-        public override void OnUpdate()
-        {
-            base.OnUpdate();
+		public override void OnUpdate()
+		{
+			if (!Settings.Instance.EnableMod)
+			{
+				if (coroutine != null) MelonCoroutines.Stop(coroutine);
+				return;
+			}
 
-            //if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.Instance.LoadKey))
-            //{
-            //    CommonUtilities.OnLoadKey();
-            //}
+			if (!GameManager.IsMainMenuActive())
+			{
+				if (Settings.Instance.AutoSaveEnabled)
+				{
+					coroutine ??= MelonCoroutines.Start(AutoSave());
+				}
 
-            if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.Instance.SaveKey))
-            {
-                CommonUtilities.OnSaveKey();
-            }
-        }
-    }
+				if (!Settings.Instance.AutoSaveEnabled)
+				{
+					MelonLogger.Msg("Autosave is not enabled, stopping Coroutine if applicable");
+					if (coroutine != null) MelonCoroutines.Stop(coroutine);
+				}
+				if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.Instance.SaveKey))
+				{
+					GameManager.SaveGameAndDisplayHUDMessage();
+				}
+
+				if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.Instance.LoadKey))
+				{
+					SaveSlotInfo ssi = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+					if (ssi == null) return;
+
+					GameManager.LoadSaveGameSlot(ssi);
+				}
+			}
+		}
+
+		public static void SAVE()
+		{
+			if (!GameManager.IsMainMenuActive())
+			{
+				GameManager.SaveGameAndDisplayHUDMessage();
+				MelonLogger.Msg("Autosave occured");
+			}
+		}
+
+		public static IEnumerator AutoSave()
+		{
+			yield return new WaitForSecondsRealtime(Settings.Instance.AutoSaveTime * 60);
+			SAVE();
+
+			MelonLogger.Msg($"Autosave completed with time of {Settings.Instance.AutoSaveTime}");
+			yield break;
+		}
+	}
 }
