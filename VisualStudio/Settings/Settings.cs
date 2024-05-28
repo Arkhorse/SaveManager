7aa2c0b7b7691909e0da3b0ba7f75d1899e2aeb1
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-28 12:06:41 -0300

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -15,10 +15,13 @@
 		[Description("Allows you to disable the 'Saving' icon on the right of the screen")]
 		public bool SaveIconEnabled          = true;
 
+		[Name("Debug")]
+		public bool DEBUG = false;
+
 		[Section("Hotkeys")]
 
 		[Name("Hotkey Preset")]
-		public HotkeyPreset hotkeyPreset    = HotkeyPreset.Vanilla;
+		public HotkeyPreset Preset    = HotkeyPreset.Vanilla;
 
 		[Name("Save")]
 		public KeyCode SaveKey               = KeyCode.F5;
@@ -32,33 +35,39 @@
 		public bool AutoSaveEnabled = false;
 
 		[Name("Time in minutes")]
+		[Description("Set this to how long you want the autosave to wait between saves. A value of 0 disables the autosave")]
 		[Slider(0,60,61)]
-		public int AutoSaveTime = 0;
+		public int AutoSaveTime = 15;
 
 		protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
 		{
+			if (field.Name == nameof(EnableMod) || field.Name == nameof(AutoSaveEnabled))
+			{
+				Main.UpdateAutosave(false);
+			}
 			if ( InterfaceManager.GetPanel<Panel_SaveIcon>() != null && EnableMod && field.Name == nameof(SaveIconEnabled) )
 			{
 				InterfaceManager.GetPanel<Panel_SaveIcon>().Enable(SaveIconEnabled);
 			}
 			if (field.Name == nameof(AutoSaveTime))
 			{
-				MelonCoroutines.Stop(SaveManager.coroutine);
-				SaveManager.coroutine = MelonCoroutines.Start(SaveManager.AutoSave());
+				if (Instance.AutoSaveTime == 0)
+				{
+					AutoSaveEnabled = false;
+					Main.UpdateAutosave(false);
+				}
+				Main.RestartAutosave();
 			}
-		}
-
-		internal void DisableSettings()
-		{
-			SetFieldVisible(nameof(AutoSaveEnabled), false);
-			SetFieldVisible(nameof(AutoSaveTime), false);
-			SetFieldVisible(nameof(LoadKey), false);
+			if (field.Name == nameof(DEBUG))
+			{
+				if (Instance.DEBUG) Main.Logger.AddLevel(FlaggedLoggingLevel.Debug);
+				else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Debug);
+			}
 		}
 
 		internal static void OnLoad()
 		{
 			Instance.AddToModSettings(BuildInfo.GUIName);
-			//Instance.DisableSettings();
 			Instance.RefreshGUI();
 		}
 	}
