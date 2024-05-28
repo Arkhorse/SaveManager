2860efb1ede06c4175a868c093fe4a26a5b77ab2
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-28 12:07:02 -0300

    1.0.5

--- VisualStudio/BuildInfo.cs
+++ VisualStudio/BuildInfo.cs
@@ -8,7 +8,7 @@
         /// <summary>Who made the mod</summary>
         public const string Author                          = "The Illusion";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "1.0.4";
+        public const string Version                         = "1.0.5";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Save Manager";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
@@ -24,11 +24,11 @@
         public const string DownloadLink                    = null;
         /// <summary>Copyright info</summary>
         /// <remarks>When updating the year, use the StartYear-CurrentYear format eg(Copyright © 2020-2023)</remarks>
-        public const string Copyright                       = "Copyright © 2023";
+        public const string Copyright                       = "Copyright © 2023-2024";
         /// <summary>Trademark info</summary>
         public const string Trademark                       = null;
         /// <summary>Product Name (Generally use the Name)</summary>
-        public const string Product                         = "Save Manager";
+        public const string Product                         = "SaveManager";
         /// <summary>Culture info</summary>
         public const string Culture                         = null;
         /// <summary>Priority of your mod. Most of the time you should not need to change this</summary>
