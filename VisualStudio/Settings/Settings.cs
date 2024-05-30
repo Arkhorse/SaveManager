b478cb3e2e2c1544b652a59b9def2b5687f99b4d
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-05-30 03:06:27 -0300

    add debug options section

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -15,9 +15,6 @@
 		[Description("Allows you to disable the 'Saving' icon on the right of the screen")]
 		public bool SaveIconEnabled          = true;
 
-		[Name("Debug")]
-		public bool DEBUG = false;
-
 		[Section("Hotkeys")]
 
 		[Name("Hotkey Preset")]
@@ -38,6 +35,18 @@
 		[Description("Set this to how long you want the autosave to wait between saves. A value of 0 disables the autosave")]
 		[Slider(0,60,61)]
 		public int AutoSaveTime = 15;
+
+		[Section("Debug Options")]
+
+		[Name("Use alternative load method")]
+		public bool AlternativeLoad = false;
+
+		[Name("Debug")]
+		public bool DEBUG = false;
+
+		[Name("Trace")]
+		[Description("Adds even more logging")]
+		public bool TRACE = false;
 
 		protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
 		{
@@ -63,11 +72,25 @@
 				if (Instance.DEBUG) Main.Logger.AddLevel(FlaggedLoggingLevel.Debug);
 				else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Debug);
 			}
+			if (field.Name == nameof(TRACE))
+			{
+				if (Instance.TRACE) Main.Logger.AddLevel(FlaggedLoggingLevel.Trace);
+				else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Trace);
+			}
 		}
+
+
 
 		internal static void OnLoad()
 		{
 			Instance.AddToModSettings(BuildInfo.GUIName);
+
+			if (Instance.DEBUG) Main.Logger.AddLevel(FlaggedLoggingLevel.Debug);
+			else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Debug);
+
+			if (Instance.TRACE) Main.Logger.AddLevel(FlaggedLoggingLevel.Trace);
+			else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Trace);
+
 			Instance.RefreshGUI();
 		}
 	}
