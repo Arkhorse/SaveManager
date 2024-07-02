879732f25e510a0f40d7aaee7f26885d30a81e2b
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-02 20:17:33 -0300

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -2,51 +2,44 @@
 {
 	public class Settings : JsonModSettings
 	{
-		internal static readonly Settings Instance = new();
+		internal static Settings Instance = new();
 		public enum HotkeyPreset { Vanilla, Custom }
 
 		[Section("Mod Controls")]
 
 		[Name("Toggle Mod")]
 		[Description("Turn the mod on or off")]
-		public bool EnableMod                = true;
+		public bool EnableMod					= true;
 
 		[Name("Save Icon Enabled")]
 		[Description("Allows you to disable the 'Saving' icon on the right of the screen")]
-		public bool SaveIconEnabled          = true;
+		public bool SaveIconEnabled				= true;
+
+		[Name("Maximum Save Slots")]
+		[Description("WARNING: Removing this mod after changing this value may result in lost saves. Use at your own risk. Vanilla = 25")]
+		[Slider(25, 999)]
+		public int MAXSAVESLOTS					= 25;
 
 		[Section("Hotkeys")]
 
 		[Name("Hotkey Preset")]
-		public HotkeyPreset Preset    = HotkeyPreset.Vanilla;
+		public HotkeyPreset Preset				= HotkeyPreset.Vanilla;
 
 		[Name("Save")]
-		public KeyCode SaveKey               = KeyCode.F5;
+		public KeyCode SaveKey					= KeyCode.F5;
 
 		[Name("Load")]
-		public KeyCode LoadKey               = KeyCode.F6;
+		public KeyCode LoadKey					= KeyCode.F6;
 
 		[Section("Autosave")]
 
 		[Name("Enabled")]
-		public bool AutoSaveEnabled = false;
+		public bool AutoSaveEnabled				= false;
 
 		[Name("Time in minutes")]
 		[Description("Set this to how long you want the autosave to wait between saves. A value of 0 disables the autosave")]
 		[Slider(0,60,61)]
-		public int AutoSaveTime = 15;
-
-		[Section("Debug Options")]
-
-		[Name("Use alternative load method")]
-		public bool AlternativeLoad = false;
-
-		[Name("Debug")]
-		public bool DEBUG = false;
-
-		[Name("Trace")]
-		[Description("Adds even more logging")]
-		public bool TRACE = false;
+		public int AutoSaveTime					= 15;
 
 		protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
 		{
@@ -67,30 +60,17 @@
 				}
 				Main.RestartAutosave();
 			}
-			if (field.Name == nameof(DEBUG))
-			{
-				if (Instance.DEBUG) Main.Logger.AddLevel(FlaggedLoggingLevel.Debug);
-				else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Debug);
-			}
-			if (field.Name == nameof(TRACE))
-			{
-				if (Instance.TRACE) Main.Logger.AddLevel(FlaggedLoggingLevel.Trace);
-				else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Trace);
-			}
 		}
 
-
+		protected override void OnConfirm()
+		{
+			base.OnConfirm();
+			Main.UpdateSaveSlotLimit(Instance.MAXSAVESLOTS);
+		}
 
 		internal static void OnLoad()
 		{
 			Instance.AddToModSettings(BuildInfo.GUIName);
-
-			if (Instance.DEBUG) Main.Logger.AddLevel(FlaggedLoggingLevel.Debug);
-			else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Debug);
-
-			if (Instance.TRACE) Main.Logger.AddLevel(FlaggedLoggingLevel.Trace);
-			else Main.Logger.RemoveLevel(FlaggedLoggingLevel.Trace);
-
 			Instance.RefreshGUI();
 		}
 	}
